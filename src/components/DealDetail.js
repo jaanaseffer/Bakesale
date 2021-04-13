88f68d28ecf156e31c5122c982178f0a1d3b326a
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2021-04-13 16:22:51 +0300

    first

---
+++ src/components/DealDetail.js
@@ -0,0 +1,173 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  PanResponder,
+  Animated,
+  Dimensions,
+  Button,
+  Linking, ScrollView,
+} from "react-native";
+import PropTypes from "prop-types";
+import { priceDisplay } from "../util";
+import ajax from "../ajax";
+
+class DealDetail extends React.Component {
+  imageXPos = new Animated.Value(0);
+  imagePanResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, gs) => {
+      this.imageXPos.setValue(gs.dx);
+    },
+    onPanResponderRelease: (evt, gs) => {
+      this.width = Dimensions.get("window").width;
+      if (Math.abs(gs.dx) > this.width * 0.4) {
+        const direction = Math.sign(gs.dx);
+        // Swipe left -1, swipe right 1
+        Animated.timing(this.imageXPos, {
+          toValue: direction * this.width,
+          duration: 250,
+        }).start(() => this.handleSwipe(-1 * direction));
+      } else {
+        Animated.spring(this.imageXPos, {
+          toValue: 0,
+        }).start();
+      }
+    },
+  });
+
+  handleSwipe = (indexDirection) => {
+    if (!this.state.deal.media[this.state.imageIndex + indexDirection]) {
+      Animated.spring(this.imageXPos, {
+        toValue: 0,
+      }).start();
+      return;
+    }
+    this.setState((prevState) => ({
+      imageIndex: prevState.imageIndex + indexDirection,
+    }), () => {
+      // Next image animation
+      this.imageXPos.setValue(indexDirection * this.width);
+      Animated.spring(this.imageXPos, {
+        toValue: 0,
+      }).start();
+    });
+  };
+
+  static propTypes = {
+    initialDealData: PropTypes.object.isRequired,
+    onBack: PropTypes.func.isRequired,
+  };
+  state = {
+    deal: this.props.initialDealData,
+    imageIndex: 0,
+  };
+
+  async componentDidMount() {
+    const fullDeal = await ajax.fetchDealDetail(this.state.deal.key);
+    this.setState({
+      deal: fullDeal,
+    });
+  }
+
+  openDealUrl = () => {
+    Linking.openURL(this.state.deal.url);
+  };
+
+  render() {
+    const { deal } = this.state;
+    return (
+      <View style={styles.deal}>
+        <TouchableOpacity onPress={this.props.onBack}>
+          <Text style={styles.backLink}>Back</Text>
+        </TouchableOpacity>
+        <Animated.Image {...this.imagePanResponder.panHandlers} source={{ uri: deal.media[this.state.imageIndex] }}
+                        style={[{ left: this.imageXPos }, styles.image]}
+        />
+        <View>
+          <Text style={styles.title}>{deal.title}</Text>
+        </View>
+        <ScrollView style={styles.detail}>
+          <View style={styles.row}>
+            <View style={styles.footer}>
+              <View style={styles.info}>
+                <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+                <Text style={styles.cause}>{deal.cause.name}</Text>
+              </View>
+            </View>
+            {deal.user && (
+              <View style={styles.user}>
+                <Image source={{ uri: deal.user.avatar }} style={styles.avatar} />
+                <Text>{deal.user.name}</Text>
+              </View>
+            )}
+          </View>
+          <View style={styles.description}>
+            <Text>{deal.description}</Text>
+          </View>
+          <Button title="Buy this deal!" onPress={this.openDealUrl} />
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create(
+  {
+    deal: {
+      marginBottom: 20,
+    },
+    image: {
+      width: "100%",
+      height: 150,
+      backgroundColor: "#ccc",
+    },
+    row: {
+      flexDirection: "row",
+      justifyContent: "space-around",
+    },
+    backLink: {
+      marginBottom: 5,
+      color: "#22f",
+      marginLeft: 10,
+    },
+    title: {
+      fontSize: 16,
+      padding: 10,
+      fontWeight: "bold",
+      backgroundColor: "rgba(237, 149, 45, 0.4)",
+    },
+    footer: {
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
+      marginTop: 15,
+    },
+    info: {
+      alignItems: "center",
+    },
+    price: {
+      fontWeight: "bold",
+    },
+    avatar: {
+      width: 60,
+      height: 60,
+      borderRadius: 50,
+    },
+    user: {
+      alignItems: "center",
+      marginTop: 15,
+    },
+    description: {
+      borderColor: "#bbb",
+      borderWidth: 1,
+      margin: 5,
+      padding: 3,
+    },
+  }
+);
+
+export default DealDetail;
